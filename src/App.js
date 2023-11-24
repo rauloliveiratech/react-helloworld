a66cd5629e7af292a7c515cfb9f8c6d176c605a7
rauloliveiratech <rauloliveiratech@gmail.com>
2023-11-24 15:21:50 -0300

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Com React JS</p>
+      <p>Criado por Raul Oliveira</p>
+
+</div>
+  );
+}
+
+export default App;
